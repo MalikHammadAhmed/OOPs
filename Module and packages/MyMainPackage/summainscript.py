d90aad80df04ba1dc6f9200b9a6859639329381a
Malik Hammad Ahmed <malik_hammad20@hotmail.com>
2023-06-13 20:39:52 +0100

    module and packages example added

---
+++ Module and packages/MyMainPackage/summainscript.py
@@ -0,0 +1,2 @@
+def report_main():
+    print(" Hey I am a in some main script i n main package")
---
+++ Module and packages/myprogram.py
@@ -0,0 +1,5 @@
+from MyMainPackage import summainscript
+from MyMainPackage.SubPackage import mysubscript
+
+summainscript.report_main()
+mysubscript.sub_report()
